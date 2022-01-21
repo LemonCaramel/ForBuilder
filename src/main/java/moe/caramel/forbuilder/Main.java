6ee46bf69d84f232e7b14a992e267db4cbf9363f
LemonCaramel <admin@caramel.moe>
2022-01-21 23:49:55 +0900

    add: event listeners

--- src/main/java/moe/caramel/forbuilder/Main.java
+++ src/main/java/moe/caramel/forbuilder/Main.java
@@ -3,11 +3,13 @@ package moe.caramel.forbuilder;
 import moe.caramel.forbuilder.command.MainCommand;
 import moe.caramel.forbuilder.command.SpeedCommand;
 import moe.caramel.forbuilder.command.WorldCommand;
+import moe.caramel.forbuilder.event.EventListener;
 import org.bukkit.WorldCreator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    public static final String DEBUG = "debug";
     public static final String WORLD_LIST = "worlds";
 
     @Override
@@ -16,6 +18,9 @@ public final class Main extends JavaPlugin {
         this.getServer().registerCommand(this, "forbuilder", new MainCommand(this));
         this.getServer().registerCommand(this, "speed", new SpeedCommand());
         this.getServer().registerCommand(this, "world", new WorldCommand(this));
+
+        // Register Event
+        this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
     }
 
     @Override
---
+++ src/main/java/moe/caramel/forbuilder/event/EventListener.java
@@ -0,0 +1,60 @@
+package moe.caramel.forbuilder.event;
+
+import moe.caramel.forbuilder.Main;
+import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerTakeLecternBookEvent;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import static moe.caramel.forbuilder.Main.DEBUG;
+
+public record EventListener(@NotNull Plugin plugin) implements Listener {
+
+    private static final NamespacedKey OVER_WORLD = NamespacedKey.minecraft("overworld");
+
+    @EventHandler
+    void onBlockBreak(BlockBreakEvent event) {
+        if (plugin.getConfig().getBoolean(DEBUG, false)) return;
+        if (event.getBlock().getWorld().getKey().equals(OVER_WORLD)) event.setCancelled(true);
+    }
+
+    @EventHandler
+    void onBlockBreak(BlockPlaceEvent event) {
+        if (plugin.getConfig().getBoolean(DEBUG, false)) return;
+        if (event.getBlock().getWorld().getKey().equals(OVER_WORLD)) event.setCancelled(true);
+    }
+
+    @EventHandler
+    void onBlockBurn(BlockBurnEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    void onBlockExplode(BlockExplodeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    void onBlockRedstone(BlockRedstoneEvent event) {
+        event.setNewCurrent(0); // No
+    }
+
+    @EventHandler
+    void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() == Action.PHYSICAL) event.setCancelled(true);
+    }
+
+    @EventHandler
+    void onPlayerTakeLecternBook(PlayerTakeLecternBookEvent event) {
+        if (!event.getPlayer().isSneaking()) event.setCancelled(true); // Toggle Sneaking
+    }
+}
