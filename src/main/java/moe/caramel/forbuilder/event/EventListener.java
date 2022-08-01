6b155915b57c35cd7809e534d99fa0389053da2d
LemonCaramel <admin@caramel.moe>
2022-08-01 19:17:53 +0900

    fix: unable to press pressure plate

--- src/main/java/moe/caramel/forbuilder/event/EventListener.java
+++ src/main/java/moe/caramel/forbuilder/event/EventListener.java
@@ -1,6 +1,6 @@
 package moe.caramel.forbuilder.event;
 
-import moe.caramel.forbuilder.Main;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,13 +24,15 @@ public record EventListener(@NotNull Plugin plugin) implements Listener {
     @EventHandler
     void onBlockBreak(BlockBreakEvent event) {
         if (plugin.getConfig().getBoolean(DEBUG, false)) return;
-        if (event.getBlock().getWorld().getKey().equals(OVER_WORLD)) event.setCancelled(true);
+        if (!event.getBlock().getWorld().getKey().equals(OVER_WORLD)) return;
+        event.setCancelled(true);
     }
 
     @EventHandler
     void onBlockBreak(BlockPlaceEvent event) {
         if (plugin.getConfig().getBoolean(DEBUG, false)) return;
-        if (event.getBlock().getWorld().getKey().equals(OVER_WORLD)) event.setCancelled(true);
+        if (!event.getBlock().getWorld().getKey().equals(OVER_WORLD)) return;
+        event.setCancelled(true);
     }
 
     @EventHandler
@@ -50,11 +52,14 @@ public record EventListener(@NotNull Plugin plugin) implements Listener {
 
     @EventHandler
     void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() == Action.PHYSICAL) event.setCancelled(true);
+        if (event.getAction() != Action.PHYSICAL) return;
+        if (event.getClickedBlock().getType() != Material.SOUL_SAND) return;
+        event.setCancelled(true);
     }
 
     @EventHandler
     void onPlayerTakeLecternBook(PlayerTakeLecternBookEvent event) {
-        if (!event.getPlayer().isSneaking()) event.setCancelled(true); // Toggle Sneaking
+        if (event.getPlayer().isSneaking()) return; // Use "Toggle Sneaking"
+        event.setCancelled(true);
     }
 }
